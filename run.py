08f4f2900104677f1c5018cdfd844157c993332a
John Rearden <reardenjohn@gmail.com>
2022-07-11 19:26:39 +0100

    Initial commit - add UniChars and AnsiCommands enums and Airfield class to run.py

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import threading
+import sys
+import enum
+
+class UniChars(enum.Enum):
+    MIDDLE_DOT = "\u00B7"
+
+class AnsiCommands(enum.Enum):
+    SAVE_CURSOR = "\x1b7"
+    RESTORE_CURSOR = "\x1b8"
+    CARRIAGE_RETURN = "\r"
+    CURSOR_TO_HOME = "\x1b[H"
+
+class Airfield:
+    """
+    Represents the airfield that the game is played on
+    """
+    def __init__(self, width, height):
+        self.cells = []
+        for _ in range(width * height):
+            self.cells.append(UniChars.MIDDLE_DOT)
+
+    def stringify(self):
+        """Converts the cells array into a printable string for stdout"""
+        return ''.join(self.cells)
+
+    def print(self):
+        """Prints the airfield string to the terminal"""
+        sys.stdout.write(AnsiCommands.SAVE_CURSOR)
+        sys.stdout.write(AnsiCommands.CURSOR_TO_HOME)
+        sys.stdout.write(self.stringify())
+        sys.stdout.write(AnsiCommands.RESTORE_CURSOR)
+        sys.stdout.flush()
+
+          
